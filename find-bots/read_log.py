e953339dbe842877162c779b9456abfe431ac91f
Redarokim Ikongha <rikongha@gmail.com>
2022-08-17 19:56:27 +0100

    updates list logic

--- find-bots/read_log.py
+++ find-bots/read_log.py
@@ -1,34 +1,34 @@
 import re
-import string
-
-bots = set()
-sessions = {}
-usernamedict = {}
-constraint = ['user logged in', 'user changed password', 'user logged off']
 
 logs = open('logs.log', 'r')
 lines = logs.readlines()
+bots = set()
+sessions = {}
+userdict  = {}
+constraint = ['user logged in', 'user changed password', 'user logged off']
 
-def add_to_username(key, value, outer_key):
-    try:
-        usernamedict[key].append(value)
-    except KeyError:
-        usernamedict[key] = [value]
-    except AttributeError:
-        usernamedict[key] = [usernamedict[key], value]
-    sessions[outer_key] = usernamedict
-
+def add_to_sessions(time, user, action):
+    if time in sessions:
+        if user in sessions[time]:
+            sessions[time][user] += action
+        else:
+            userdict[user] += action
+            sessions[time] = userdict
+    else:
+        sessions[time] = {user: [action]}
+    
 
 for line in lines:
     time = re.findall("[0-9]{2}:[0-9]{2}:[0-9]{2}", line)[0]
     userDetails = re.findall("(?<=\|).+?(?=\|)", line)
     username = userDetails[1]
     action = userDetails[3]
-    add_to_username(username, action, time)
 
-for entry in sessions:
-    for username in usernamedict:
-        if sessions[entry][username] == constraint:
-            bots.add(username)
+    add_to_sessions(time, username, action)
+
+for time, usernames in sessions.items():
+    for name, actions in usernames.items():
+        if(set(constraint)==(set(actions))):
+            bots.add(name)
 
 print("\n".join(bots))
